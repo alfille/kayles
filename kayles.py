173df57985e80f8630ac0ae3f5e85f85956e0743
paul <paul.alfille@gmail.com>
2019-12-02 12:34:55 -0500

    Change win to a dictionary of winning moves and display that
    :

--- kayles.py
+++ kayles.py
@@ -3,12 +3,22 @@
 
 # solve game from winning ways for your mathematical plays
 
-win=set()
+#Win is a dict with the repr of the position as the index and the winning move as the value
+win={}
+
+# lose is a set of losing positions with teh repr of the position as the index (set member)
 lose=set()
+
+# kill is the number of pieces that can be taken from the selected heap
 kill=[1,2]
 
-# heap array is a list of heaps of different sizes
-# big-endian 
+# heap array is a list of the number of heaps of different sizes
+# big-endian
+# i.e. [2,0,1] is 2 heaps of 3 and 1 heap of one
+#   legal moves: (heap,killsize,subheap size)
+#   (1,1,0) -> [2,0,0]
+#   (3,1,1) -> [1,0,2]
+#   (3,2,0) -> [1] (actually [0,0,1] but we take off leading zeroes)
 
 def SingleHeapArray(size):
     # Single heap, rest are empty
@@ -35,23 +45,23 @@ def TestHeapAray(heapArray):
 
     # Try all legal moves
     heaparrayLength = len(heapArray)
-    for i in range(heaparrayLength): # loop through heaps
+    for i,h in enumerate(heapArray): # loop through heaps
         
-        if heapArray[i]==0:
+        if h==0:
             # no heaps of this size
             continue
             
-        heap = heaparrayLength-i
+        heapSize = heaparrayLength-i
         for k in kill: 
             #loop through kill sizes
             
-            if k > heap:
+            if k > heapSize:
                 # heap size too small for this kill size
                 continue
                 
-            for subheap1 in range(heap-k+1): 
+            for subheap1 in range(heapSize-k+1): 
                 #loop through splits
-                subheap2=heap-k-subheap1
+                subheap2=heapSize-k-subheap1
                 
                 if subheap2 > subheap1: 
                     #redundant by symmetry
@@ -68,7 +78,7 @@ def TestHeapAray(heapArray):
                 # Test the modified position
                 if TestHeapAray(modHeapArray) == "L":
                     # made other guy a loser
-                    win.add(rp)
+                    win[rp] = (heapSize,k,subheap1)
                     print("Slow win "+rp)
                     return "W"
     # no good moves -- I'm a loser
@@ -80,14 +90,14 @@ def TestHeapAray(heapArray):
 def main():
     # salt the problem
     lose.add(repr([]))
-    
-    
+        
     TestHeapAray(SingleHeapArray(25))
-    print("win")
-    print(sorted(list(win)))
     print("lose")
     print(sorted(list(lose)))
     
+    for w in sorted(list(win)):
+        print("{}\t->\t{}".format(w,win[w]))
+
 if __name__ == "__main__":
     # execute only if run as a script
     main()
